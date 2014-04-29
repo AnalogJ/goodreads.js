9e8f58e76f2e6f983bca0720ea2ce49fcdd343a1
jason <jk17@ualberta.ca>
2014-04-29 12:13:59 -0700

    update with gitignore and public api calls.

--- example/test.js
+++ example/test.js
@@ -11,33 +11,42 @@
 
 
     var provider = new goodreads.provider({
-        'client_key': '',
-        'client_secret' : ''
+        'client_key': 'VEjXc5XWMAeIJYoHlqZK8w',
+        'client_secret' : 'sdfsdf'
     })
-    provider.oAuthGetRequestTokenUrl()
-        .then(function(resp){
-            console.log('Visit the url: ', resp.url);
-            return resp;code
-
+    provider.CreateClient()
+        .then(function(client){
+            return client.SearchForBookTitleAuthor( "Imager","L.E. Modesitt Jr.")
         })
-        .then(function(resp){
-            var deferred = Q.defer();
-            rl.question('Enter the authorize code here:', function(code){
-                deferred.resolve([resp, code])
-            });
-            return deferred.promise;
+        .then(function(data){
+            console.log("FINISHED")
+            console.log(JSON.stringify(data));
         })
-        .spread(function(resp, code){
-            console.log(arguments)
-            return provider.oAuthGetAccessToken(resp.request_token, resp.request_token_secret, code)
-        })
-        .then(function(access_tokens){
-
-            console.log(access_tokens)
-        })
-        .fail(function(err){
-            console.log(err)
-            console.log("AN ERROR OCCURED");
-        })
+        .fail(function(err){console.log(err)})
+//    provider.oAuthGetRequestTokenUrl()
+//        .then(function(resp){
+//            console.log('Visit the url: ', resp.url);
+//            return resp;code
+//
+//        })
+//        .then(function(resp){
+//            var deferred = Q.defer();
+//            rl.question('Enter the authorize code here:', function(code){
+//                deferred.resolve([resp, code])
+//            });
+//            return deferred.promise;
+//        })
+//        .spread(function(resp, code){
+//            console.log(arguments)
+//            return provider.oAuthGetAccessToken(resp.request_token, resp.request_token_secret, code)
+//        })
+//        .then(function(access_tokens){
+//
+//            console.log(access_tokens)
+//        })
+//        .fail(function(err){
+//            console.log(err)
+//            console.log("AN ERROR OCCURED");
+//        })
 
 }).call(this);
